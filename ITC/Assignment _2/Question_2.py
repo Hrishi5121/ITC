7056db1984a64ef856146e2bc17ea5b50351b387
Hrishi5121 <121247966+Hrishi5121@users.noreply.github.com>
2022-12-22 21:37:22 +0530

    Add files via upload

---
+++ ITC/Assignment _2/Question_2.py
@@ -0,0 +1,7 @@
+Name = "Hrishi Raj Paul";
+SID = 22105121;
+Dept_Name = "ECE";
+
+CGPA = 10.0;
+
+print("Hey,", Name ,"Here!\n","My SID is", SID ," \n ","I am from", Dept_Name ,"department and my CGPA is", CGPA)
